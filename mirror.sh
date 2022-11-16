89eb735f09169d5a286f4f1e4f10cd4eafad11f1
suddhasattwa-git <78218214+suddhasattwa-git@users.noreply.github.com>
2022-11-16 17:14:27 +0530

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -3,6 +3,6 @@ set -eu
 
 /setup-ssh.sh
 
-export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+export GIT_SSH_COMMAND="sshpass -p orderserv ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
